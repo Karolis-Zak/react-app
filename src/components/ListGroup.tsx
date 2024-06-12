ecea79dc0d99c72d5bd29aaf05cae86f35371893
Your Name <k.zaksauskas@rgu.ac.uk>
2024-06-12 20:56:07 +0100

    Event Handle

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
 import Message from "../Message";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Fracisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  //Event Handle
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
